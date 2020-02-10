15125d87d3e59e64818ab9a5ce3fd3f55ae436be
korolaab <gusarov.alex3@ya.ru>
2020-02-10 17:52:59 +0100

    abstract_branch files

--- compress_abstract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z $1]
-    then
-    echo "No key found."
-else
-    $(mv ./abstract/*.pdf ./)
-    $(tar -cf - abstract/ | xz -9 -c | openssl aes-256-cbc -salt -a -e -out abstract.enc -pass file:./$1)
-fi
-
--- decompress_abstract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z $1 ]
-    then
-    echo "No key found"
-else
-    $(openssl aes-256-cbc -salt -a -d -in abstract.enc  -pass file:./$1 | tar -xJf -)
-fi
-
